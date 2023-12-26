7950cd3f92e41918ca6c34da7a9060f0a022b53f
8il4l <73673899+8il4l@users.noreply.github.com>
2023-12-26 18:24:13 +0100

    m501 done

--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bgannoun <bgannoun@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 09:17:51 by bgannoun          #+#    #+#             */
-/*   Updated: 2023/12/25 15:11:29 by bgannoun         ###   ########.fr       */
+/*   Updated: 2023/12/26 18:22:51 by bgannoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,9 @@
 
 int main(void)
 {
-	// try
-	// {
-	// 	Bureaucrat a("one", 1);
-	// 	std::cout << a;
-	// 	// a.increment();
-	// 	// std::cout << a;
-	// 	// a.increment();
-		
-	// 	Bureaucrat b("two", 150);
-	// 	std::cout << b;
-	// 	// b.decrement();
-	// 	// std::cout << b;
-	// }
-	// catch(std::exception &e)
-	// {
-	// 	std::cout << e.what() << std::endl;
-	// }
-	
-	// try
-	// {
-	// 	Form f("test", 20, 20);
-	// 	Bureaucrat b("name", 20);
-	// 	std::cout << f << std::endl << b;
-	// 	f.beSigned(b);
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
 	Form f("form1", 10, 10);
-	Bureaucrat b1("b1", 10);
+	Bureaucrat b1("b1", 9);
+	
 	try{
 		f.beSigned(b1);		
 		std::cout << f << std::endl;
@@ -55,6 +27,5 @@ int main(void)
 	{
 		std::cout << e.what() << std::endl;
 	}
-	// b1.signForm(f);
 	return (0);
 }
